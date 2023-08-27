e978af3ecc2a80386e43d082a63ee7937f07335f
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-27 15:23:50 +0530

    Create howtopay.js

---
+++ commands/howtopay.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('howtopay')
+    .setDescription('Get instructions on how to pay'),
+  async execute(interaction) {
+    await interaction.deferReply()
+    const embed = new EmbedBuilder()
+      .setTitle(interaction.user.username)
+      .setDescription('Success')
+      .setColor('Random') // Yellowish color
+      .setThumbnail(interaction.user.displayAvatarURL())
+      .setFooter({
+        text: interaction.guild.name,
+        iconURL: interaction.guild.iconURL(),
+      });
+
+    try {
+      await interaction.followUp({ embeds: [embed] });
+    } catch (error) {
+      console.error(error);
+      await interaction.followUp({ content: 'An error occurred while sending the response.', ephemeral: true });
+    }
+  },
+};
+
