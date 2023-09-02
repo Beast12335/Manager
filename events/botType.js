73c6302c1d67e7d5bba7a1e0e3f1cee3664dbd99
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-09-02 13:29:45 +0530

    Update botType.js

--- events/botType.js
+++ events/botType.js
@@ -26,29 +26,16 @@ module.exports = {
       const type = interaction.values[0]
       const embed = interaction.message.embeds[0]
       const newEmbed = EmbedBuilder.from(embed).addFields({name:`**Bot Creator:**`,value: interaction.user.id,inline:true},{name:`**Bot Type:**`,value:type,inline:true})
-      await interaction.editReply({embeds:[newEmbed], components:[]});
-      const msg = await interaction.channel.send('Please enter the **Bot Name**. You have 2 minutes to do so')
-      const collector= interaction.channel.createMessageCollector({time:120000})
-      collector.on('collect', async(m) =>{
-        collector.stop()
-        const emb = EmbedBuilder.from(newEmbed).addFields({name:`**Bot Name:**`,value:m.content});
-        await interaction.editReply({embeds:[emb]})
-        await m.delete()
-        await msg.delete()
-        });
-      collector.on('end', async(collected) =>{
-        if(collected.size == '0'){
-          await interaction.channel.send('Time Out. Click on the restart button to continue further')
-          const button = new ButtonBuilder()
-          .setStyle('Secondary')
-          .setLabel('Restart')
-          .setCustomId('restart')
-          const row = new ActionRowBuilder()
-          .addComponents(button)
-          await interaction.channel.send({embeds:[newEmbed], components:[row]})
-          await interaction.deleteReply()
-          }
-        });
+      const menu = new StringSelectMenuBuilder()
+      .setPlaceholder('Choose the payment type')
+      .setCustomId('payment')
+      .addOptions({
+        label:`Invites`,value:`invites`},{
+        label:`Money`,value:`money`},{
+        label:`Event`,value:`event`});
+      const row = new ActionRowBuilder()
+      .addComponents(menu)
+      await interaction.editReply({embeds:[newEmbed], components:[row]});
     } catch (e) {
       console.log('Error handling  bot creation type menu:', e);
       await interaction.followUp({content:`Error: ${e}`});
