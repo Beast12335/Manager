8fad24a938bdc95129d3af334e685fe8478e4266
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-09-03 15:25:48 +0530

    Update helper.js

--- events/helper.js
+++ events/helper.js
@@ -1,14 +1,49 @@
 async function name(interaction,EmbedBuilder,ButtonBuilder,ActionRowBuilder){
   const u = await interaction.channel.send('Please send the **Bot Name**. You have 2 minutes to do so')
-  console.log('name')
   const collector = await interaction.channel.createMessageCollector({time:120000})
   collector.on('collect', async(m) =>{
     collector.stop()
     const newEmbed = interaction.message.embeds[0]
-    const emb = EmbedBuilder.from(newEmbed).addFields({name:`**Demoo:**`,value:m.content});
-    await interaction.editReply({embeds:[emb]})
+    const emb = EmbedBuilder.from(newEmbed).addFields({name:`**Bot Name:**`,value:m.content});
+    await interaction.editReply({embeds:[emb], components:[]})
     await m.delete()
     await u.delete()
+    
+    const p = await interaction.channel.send('Please send the **Bot Id**. You have 2 minutes to do so')
+    const collect = await interaction.channel.createMessageCollector({time:120000})
+    collect.on('collect', async(t) =>{
+      collect.stop()
+      const nEmbed = interaction.message.embeds[0]
+      const emb = EmbedBuilder.from(nEmbed).addFields({name:`**Bot Id:**`,value:t.content});
+      const button = new ButtonBuilder()
+      .setStyle('Link')
+      .setLabel('Invite Bot')
+      .setURL('https://www.google.com');
+      const confirm = new ButtonBuilder()
+      .setStyle('Success')
+      .setLabel('Confirm')
+      .setCustomId('confirm_details');
+      const row = new ActionRowBuilder()
+      .addComponents(button,confirm)
+      await interaction.editReply({embeds:[emb], components:[row]})
+      await p.delete()
+      await t.delete()
+      });
+    collect.on('end', async(collected) =>{
+      if(collected.size == '0'){
+        await interaction.channel.send('Time Out. Click on the restart button to continue further')
+        const button = new ButtonBuilder()
+        .setStyle('Secondary')
+        .setLabel('Restart')
+        .setCustomId('restart')
+        const row = new ActionRowBuilder()
+        .addComponents(button)
+        await interaction.channel.send({embeds:[newEmbed], components:[row]})
+        await interaction.deleteReply()
+        await p.delete()
+        await t.delete()
+        }
+      });
     });
   collector.on('end', async(collected) =>{
     if(collected.size == '0'){
@@ -27,43 +62,4 @@ async function name(interaction,EmbedBuilder,ButtonBuilder,ActionRowBuilder){
     });
 }
 
-async function id(interaction,EmbedBuilder,ButtonBuilder,ActionRowBuilder){
-  const p = await interaction.channel.send('Please send the **Bot Id**. You have 2 minutes to do so')
-  console.log('id')
-  const collector = await interaction.channel.createMessageCollector({time:120000})
-  collector.on('collect', async(t) =>{
-    collector.stop()
-    const newEmbed = interaction.message.embeds[0]
-    const emb = EmbedBuilder.from(newEmbed).addFields({name:`**Id:**`,value:t.content});
-    const button = new ButtonBuilder()
-    .setStyle('Link')
-    .setLabel('Invite Bot')
-    .setURL('https://www.google.com');
-    const confirm = new ButtonBuilder()
-    .setStyle('Success')
-    .setLabel('Confirm')
-    .setCustomId('confirm_details');
-    const row = new ActionRowBuilder()
-    .addComponents(button,confirm)
-    await interaction.editReply({embeds:[emb], components:[row]})
-    await p.delete()
-    await t.delete()
-    });
-  collector.on('end', async(collected) =>{
-    if(collected.size == '0'){
-      await interaction.channel.send('Time Out. Click on the restart button to continue further')
-      const button = new ButtonBuilder()
-      .setStyle('Secondary')
-      .setLabel('Restart')
-      .setCustomId('restart')
-      const row = new ActionRowBuilder()
-      .addComponents(button)
-      await interaction.channel.send({embeds:[newEmbed], components:[row]})
-      await interaction.deleteReply()
-      await p.delete()
-      await t.delete()
-      }
-    });
-}
-module.exports = { name,id };
-//module.exports = { id };
+module.exports = { name };
