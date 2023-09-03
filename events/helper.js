b05fc112c78f3c894e6be1878a5906498b8452dd
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-09-03 14:42:00 +0530

    Update helper.js

--- events/helper.js
+++ events/helper.js
@@ -1,8 +1,9 @@
-async function name(interaction){
+async function name(interaction,EmbedBuilder,ButtonBuilder,ActionRowBuilder){
   const u = await interaction.channel.send('Please send the **Bot Name**. You have 2 minutes to do so')
   const collector = await interaction.channel.createMessageCollector({time:120000})
   collector.on('collect', async(m) =>{
     collector.stop()
+    const newEmbed = interaction.message.embeds[0]
     const emb = EmbedBuilder.from(newEmbed).addFields({name:`**Demoo:**`,value:m.content});
     await interaction.editReply({embeds:[emb]})
     await m.delete()
@@ -24,4 +25,32 @@ async function name(interaction){
       }
     });
 }
-module.exports = { name };
+
+async function id(interaction,EmbedBuilder,ButtonBuilder,ActionRowBuilder){
+  const u = await interaction.channel.send('Please send the **Bot Id**. You have 2 minutes to do so')
+  const collector = await interaction.channel.createMessageCollector({time:120000})
+  collector.on('collect', async(m) =>{
+    collector.stop()
+    const newEmbed = interaction.message.embeds[0]
+    const emb = EmbedBuilder.from(newEmbed).addFields({name:`**Id:**`,value:m.content});
+    await interaction.editReply({embeds:[emb]})
+    await m.delete()
+    await u.delete()
+    });
+  collector.on('end', async(collected) =>{
+    if(collected.size == '0'){
+      await interaction.channel.send('Time Out. Click on the restart button to continue further')
+      const button = new ButtonBuilder()
+      .setStyle('Secondary')
+      .setLabel('Restart')
+      .setCustomId('restart')
+      const row = new ActionRowBuilder()
+      .addComponents(button)
+      await interaction.channel.send({embeds:[newEmbed], components:[row]})
+      await interaction.deleteReply()
+      await u.delete()
+      await m.delete()
+      }
+    });
+}
+module.exports = { name,id };
