f9371bcc68a5ad44ac5f9295b0e8fd14a4171d17
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-09-06 14:20:16 +0530

    Create paymentRemove.js

---
+++ commands/paymentRemove.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
+const mysql = require('mysql2/promise');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('payment-remoce')
+    .setDescription('Deletes all payment records for a bot(admins only)')
+    .addUserOption(option =>
+    option.setName('bot')
+    .setDescription('Choose the bot')
+    .setRequired(true)),
+  async execute(interaction) {
+    await interaction.deferReply()
+    // Check if the user is an administrator
+    if (!interaction.member.permissions.has('ADMINISTRATOR')) {
+      return interaction.reply('You do not have permission to use this command.');
+    }
+
+    try {
+      const connection = await mysql.createConnection(process.env.DB_URL);
+      const bot = interaction.options.getUserOptipn('bot').id
+      
+      const [rows] = await connection.execute('SELECT * FROM bots_db where id = ?',[bot]);
+
+      if (rows.length === 0) {
+        return interaction.followUp('Not a valid bot.');
+      }
+      await connection.execute('delete from payments where bot = ?',[bot])
+      const embed = new EmbedBuilder()
+          .setTitle('Payment Removed')
+          .setColor(`#${Math.floor(Math.random() * 16777215).toString(16)}`)
+          .setDescription(`All payment records for <@${bot}> have been removed.`);
+
+        await interaction.followUp({ embeds: [embed] });
+    } catch (error) {
+      console.error(error);
+      await interaction.followUp('An error occurred while fetching bot records.');
+    }
+  },
+};
