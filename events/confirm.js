c867be0e83a8746da0f15d07a1ab4829d694b050
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-09-06 17:21:30 +0530

    Update confirm.js

--- events/confirm.js
+++ events/confirm.js
@@ -73,7 +73,7 @@ To get started type \`/help\` \n
       const msg = await customer.send({content:message,embeds:[dmEmbed],files:[file]})
       await msg.pin()
       await customer.send({content:`<@${embed.fields[1].value}> Created By: ${creator} | ${embed.fields[2].value}`,embeds:[orderEmbed]})
-      await connection.execute('insert into bots_db values(?,?,?,?,?,?,?)',[embed.fields[1].value,embed.fields[2].value,embed.fields[3].value,embed.fields[4].value,embed.fields[5].value,embed.fields[6].value,embed.fields[7].value])
+      await connection.execute('insert into bots_db values(?,?,?,?,?,?,?,?)',[embed.fields[1].value,embed.fields[2].value,embed.fields[3].value,embed.fields[4].value,embed.fields[5].value,embed.fields[6].value,embed.fields[7].value,embed.fields[8].value])
 } catch (error) {
       console.error('Error handling confirm bot creation button interaction:', error);
       if (error.code == 50007){
