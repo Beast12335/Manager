22dcce20623119d612b7bb873e19f8cc98561ed3
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-09-16 09:11:33 +0530

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -6,13 +6,34 @@ module.exports = {
     .setType(ApplicationCommandType.Message),
   async execute(interaction) {
     await interaction.deferReply({ephemeral:true})
-    const embed = new EmbedBuilder()
-      .setColor('Random')
-      .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
-      .addFields({name:'Ping', value:`${interaction.client.ws.ping}ms`})
-      .setFooter({text:interaction.guild.name, iconURL:interaction.guild.iconURL()});
-    console.log(interaction)
-    console.log(interaction.targetMessage)
-    await interaction.followUp({ embeds: [embed] });
+    if (!interaction.member.permissions.has('ADMINISTRATOR')) {
+      return interaction.followUp('You do not have permission to use this command.');
+    }
+    console.log(interaction.targetMessage.components)
+    const message = interaction.targetMessage
+    if (message.components && message.components.length > 0) {
+      // Find the index of the option with the same name as the command
+      const optionIndex = message.components[0].components.findIndex(
+        (component) => component.customId === interaction.commandName
+      );
+    
+      // If the option exists, remove it
+      if (optionIndex !== -1) {
+        message.components[0].components.splice(optionIndex, 1);
+    
+        // Edit the message to update the select menu
+        await message.edit({components: [message.components[0]]});
+    
+        interaction.followUp(
+          `Removed the ${interaction.commandName} option from the select menu.`
+        );
+      } else {
+        interaction.followUp(
+          `The ${interaction.commandName} option was not found in the select menu.`
+        );
+      }
+    } else {
+      interaction.followUp('The message does not contain a select menu.');
+    };
   },
 }
