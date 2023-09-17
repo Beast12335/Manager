ea0e602f9a2caf1d623baccdc53207bc71819ade
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-09-17 08:16:03 +0530

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,24 +2,24 @@ const { EmbedBuilder,ContextMenuCommandBuilder, ApplicationCommandType } = requi
 
 module.exports = {
   data: new ContextMenuCommandBuilder()
-    .setName('dw')
+    .setName('toggle')
     .setType(ApplicationCommandType.Message),
   async execute(interaction) {
     await interaction.deferReply({ephemeral:true})
     if (!interaction.member.permissions.has('ADMINISTRATOR')) {
       return interaction.followUp('You do not have permission to use this command.');
     }
-    console.log(interaction.targetMessage.components[0].components[0].options)
+    console.log(interaction.targetMessage.components)
     const message = interaction.targetMessage
     if (message.components && message.components.length > 0) {
       // Find the index of the option with the same name as the command
-      const optionIndex = message.components[0].components[0].options.findIndex(
-        (component) => component.value == interaction.commandName
+      const optionIndex = message.components[0].components.findIndex(
+        (component) => component.customId === interaction.commandName
       );
     
       // If the option exists, remove it
       if (optionIndex !== -1) {
-        message.components[0].components[0].options.splice(optionIndex, 1);
+        message.components[0].components.splice(optionIndex, 1);
     
         // Edit the message to update the select menu
         await message.edit({components: [message.components[0]]});
@@ -28,6 +28,8 @@ module.exports = {
           `Removed the ${interaction.commandName} option from the select menu.`
         );
       } else {
+        message.components[0].components[0].options.splice(0,0,{label:`dw`,value:`dw`})
+        await message.edit({components:[message.components[0]]})
         interaction.followUp(
           `The ${interaction.commandName} option was not found in the select menu.`
         );
